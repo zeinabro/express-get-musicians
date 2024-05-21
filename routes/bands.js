b10fde311588b3829c24f3fa043fa86bf6c7f034
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-21 11:01:26 +0100

    bands router

---
+++ routes/bands.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const { Band } = require("../models")
+
+const bandRouter = express.Router()
+
+bandRouter.get("/", async(req,res) => {
+    const bands = await Band.findAll()
+    res.json(bands)
+})
+
+bandRouter.get("/:id", async(req,res) => {
+    const band = await Band.findByPk(req.params.id)
+    res.json(band)
+})
+
+bandRouter.use(express.json())
+bandRouter.use(express.urlencoded())
+
+bandRouter.post("/", async(req,res) => {
+    const band = await Band.create({
+        name: req.body.name,
+        genre: req.body.genre
+    })
+    res.json(band)
+})
+
+bandRouter.put("/:id", async(req,res) => {
+    await Band.update({
+        name: req.body.name,
+        genre: req.body.genre
+    },{
+        where: {
+            id: req.params.id
+        }
+    })
+    const band = await Band.findByPk(req.params.id)
+    res.json(band)
+})
+
+bandRouter.delete("/:id", async(req,res) => {
+    await Band.destroy({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.send("Deleted successfully")
+})
+
+module.exports = bandRouter
--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const app = express();
 const { Musician } = require("../models/index")
 const { db } = require("../db/connection");
 const musicianRouter = require("../routes/musicians");
+const bandRouter = require("../routes/bands");
 
 app.use("/musicians", musicianRouter)
+app.use("/bands", bandRouter)
 
 module.exports = app;
